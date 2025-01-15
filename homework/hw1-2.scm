2e4186e7cf02c24fe92b219f9d66497cf80f94e7
Vastille Dolcine <vdolcine@gmail.com>
2025-01-14 22:19:36 -0800

    starting the week 1 homework

---
+++ homework/hw1-2.scm
@@ -0,0 +1,11 @@
+#lang simply-scheme
+
+(define (squares nums)
+  (define (square x) (* x x))
+  (if (empty? nums)
+      '()
+      (se (square (first nums))
+          (squares (bf nums)))))
+
+
+(squares '(2 3 4 5))
